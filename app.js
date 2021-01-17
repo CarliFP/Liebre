d4526e06ca19754ce137cbccb8cc5d33bf7fbf27
CarliFP <carlafreschpons@gmail.com>
2021-01-17 19:35:42 -0300

    Imagenes agregadas del Repo de Santi

--- app.js
+++ app.js
@@ -6,15 +6,18 @@ const app = express();
 //console.log (path)
 //console.log (app)
 
+app.use(express.static('public'));
+
 app.listen (3000, () => {
-    console.log ('Levantando un servidor con Express')
+    console.log ('Levantando un servidor con Express: Puerto 3000')
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/index.html'));
+    res.sendFile(path.resolve(__dirname, '/views/index.html'));
 });
 
 app.get('/404', (req, res) => {
       res.send("Error página no encontrada", 404);
     //console.log ();
 });
+
