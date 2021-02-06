01ae429342f5df271366bda672b679583faf9c01
CarliFP <carlafreschpons@gmail.com>
2021-02-06 01:27:16 -0300

    app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.static("public"));
 
-app.listen(process.env.PORT || 3000, function() {
+app.listen(process.env.PORT || 3000, () => {
     console.log('Servidor corriendo en el puerto 3000'); 
 })
 
