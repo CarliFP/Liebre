197044e8636e3e180ef587fff29d2bacc97cd083
CarliFP <carlafreschpons@gmail.com>
2021-02-05 21:59:53 -0300

    nuevo app.js

--- app.js
+++ app.js
@@ -1,42 +1,11 @@
 const express = require ("express");
 const path = require ("path");
 const app = express(); 
+const port = Process.env.PORT || 3000;
 
-//console.log (express)
-//console.log (path)
-//console.log (app)
 
 app.use(express.static("public"));
 
-/*app.listen (3000, () => {
-    console.log ("Levantando un servidor con Express: Puerto 3000")
-});*/
-
-/*
-app.get('/', (req, res) => {
-    //res.sendFile(path.join(__dirname, '/views/index.html'));
-    res.sendFile(path.resolve('./views/index.html'));
-    //otra forma: console.log(path.join(__dirname, '/public/hola.html'))
-    //console.log("A: "+__dirname+"/views/index.html")
-    //console.log("B: /Users/cfpons/Desktop/Liebre/views/index.html")
-});
-
-app.get('/register', (req, res) => {
-    res.sendFile(path.resolve('./views/register.html'));
-    //console.log ();
-});
-
-
-app.get('/login', (req, res) => {
-    res.sendFile(path.resolve('./views/login.html'));
-    //console.log ();
-});
-
-
-app.get('/404', (req, res) => {
-      res.send("Error página no encontrada", 404);
-    //console.log ();
-}); */
 
 app.listen(process.env.PORT || 3000, function() {
     console.log('Servidor corriendo en el puerto 3000'); 
