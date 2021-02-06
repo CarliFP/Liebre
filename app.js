13e867ec4b4fab22cca30d87b69ebf8773209e77
CarliFP <carlafreschpons@gmail.com>
2021-02-05 22:24:18 -0300

    url modificado

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ const app = express();
 
 app.use(express.static("public"));
 
-
 app.listen(process.env.PORT || 3000, function() {
     console.log('Servidor corriendo en el puerto 3000'); 
 })
+
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve('./views/index.html'));
